2e811a1a45878fa86dc7ee257d3b9788bf51e0da
yohanajocelyn <yohanajoycelyn@gmail.com>
2024-12-12 11:58:41 +0700

    fix splash screen

--- app/src/main/java/com/yohana/echolearn/views/SplashScreenView.kt
+++ app/src/main/java/com/yohana/echolearn/views/SplashScreenView.kt
@@ -2,10 +2,16 @@ package com.yohana.echolearn.views
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -14,6 +20,8 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.yohana.echolearn.R
 import com.yohana.echolearn.ui.theme.EchoLearnTheme
 
@@ -30,16 +38,24 @@ fun SplashScreenView(){
         modifier = Modifier
             .fillMaxSize()
             .background(color = Color(0xFF3DB2FF))
+        ,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ){
         Image(
             painter = painterResource(id = R.drawable.app_logo),
-            contentDescription = "App Logo"
+            contentDescription = "App Logo",
+            modifier = Modifier
+                .width(250.dp)
         )
         Text(
             text = "Echo Learn",
             color = Color.White,
             fontFamily = poppins,
-            fontWeight = FontWeight.Bold,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(top = 16.dp)
         )
     }
 }
