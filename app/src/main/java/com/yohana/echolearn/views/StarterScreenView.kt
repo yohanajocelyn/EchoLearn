bee5f100170b89590eb535557695e843e9ea63ed
yohanajocelyn <yohanajoycelyn@gmail.com>
2024-12-12 12:11:18 +0700

    update starterpage

--- app/src/main/java/com/yohana/echolearn/views/StarterScreenView.kt
+++ app/src/main/java/com/yohana/echolearn/views/StarterScreenView.kt
@@ -1,8 +1,30 @@
 package com.yohana.echolearn.views
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.yohana.echolearn.Greeting
+import com.yohana.echolearn.ui.theme.EchoLearnTheme
 
 @Composable
 fun StarterScreenView(){
-    
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ){
+
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun StarterScreenPreview() {
+    EchoLearnTheme {
+        StarterScreenView()
+    }
 }
