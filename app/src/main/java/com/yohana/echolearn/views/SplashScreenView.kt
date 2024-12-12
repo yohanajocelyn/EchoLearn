4da56e65077578ef781b7a1a69165ea3d55c276d
yohanajocelyn <yohanajoycelyn@gmail.com>
2024-12-12 12:32:43 +0700

    Update SplashScreenView.kt

--- app/src/main/java/com/yohana/echolearn/views/SplashScreenView.kt
+++ app/src/main/java/com/yohana/echolearn/views/SplashScreenView.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.yohana.echolearn.R
 import com.yohana.echolearn.ui.theme.EchoLearnTheme
+import com.yohana.echolearn.ui.theme.poppins
 
 @Composable
 fun SplashScreenView(){
