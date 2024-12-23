d94df9ab4c1dd42a2e9f1e6e0191e21aca95752a
yohanajocelyn <yohanajoycelyn@gmail.com>
2024-12-23 13:21:01 +0700

    fix splashscreen

--- app/src/main/java/com/yohana/echolearn/route/AppRouting.kt
+++ app/src/main/java/com/yohana/echolearn/route/AppRouting.kt
@@ -8,13 +8,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.yohana.echolearn.Greeting
+import com.yohana.echolearn.views.SplashScreenView
 import kotlinx.coroutines.delay
 
 fun isFirstTimeLaunch(context: Context): Boolean {
@@ -28,26 +33,9 @@ fun setFirstTimeLaunch(context: Context) {
 }
 
 @Composable
-fun SplashScreen(onTimeout: () -> Unit){
-    LaunchedEffect(Unit) {
-        delay(3000L) // Show for 3 seconds
-        onTimeout()
-    }
-
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = "Welcome to My App"
-        )
-    }
-}
-
-@Composable
 fun AppRouting(context: Context, innerpadding: PaddingValues){
     val navController = rememberNavController()
-    val isFirstLaunch = remember { isFirstTimeLaunch(context) }
+    var isFirstLaunch by rememberSaveable { mutableStateOf(true) }
 
     NavHost(
         navController = navController,
@@ -55,7 +43,7 @@ fun AppRouting(context: Context, innerpadding: PaddingValues){
         modifier = Modifier.padding(innerpadding)
     ){
         composable(route = "splash"){
-            SplashScreen {
+            SplashScreenView {
                 // Mark the first launch as completed
                 setFirstTimeLaunch(context)
                 navController.navigate("home") {
