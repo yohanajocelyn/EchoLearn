d0b90b224c1113d95c5a8c6ef6779a9d58df85a6
yohanajocelyn <yohanajoycelyn@gmail.com>
2024-12-25 12:45:19 +0700

    Merge branch 'homeview'

--- app/src/main/java/com/yohana/echolearn/views/HomeView.kt
+++ app/src/main/java/com/yohana/echolearn/views/HomeView.kt
@@ -6,22 +6,27 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -36,7 +41,7 @@ import com.yohana.echolearn.ui.theme.poppins
 fun HomeView(
     name: String = "Android"
 ){
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .background(color = Color(0xFF3DB2FF))
@@ -44,6 +49,7 @@ fun HomeView(
         ,
     ){
         Greeting(name = name)
+        LearningMenu()
     }
 }
 
@@ -52,6 +58,7 @@ fun Greeting(name: String){
     Row (
         modifier = Modifier
             .fillMaxWidth()
+            .fillMaxHeight()
             .padding(start = 18.dp)
         ,
         horizontalArrangement = Arrangement.SpaceBetween,
@@ -98,42 +105,121 @@ fun LearningMenu(){
     Column (
         modifier = Modifier
             .fillMaxWidth()
-            .background(color = Color.White)
             .clip(
                 RoundedCornerShape(
-                    topStart = 16.dp,
-                    topEnd = 16.dp,
+                    topStart = 13.dp,
+                    topEnd = 13.dp,
                 )
             )
+            .background(color = Color.White)
+            .padding(top = 42.dp)
         ,
+        horizontalAlignment = Alignment.CenterHorizontally
     ){
+        LearningCard(
+            mainColor = Color(0xFF3DB2FF),
+            shadowColor = Color(0xFF263238),
+            menu = "Speaking",
+            content = "Listen and shadow select lyrics to perfect your pronunciation!",
+            img = painterResource(id = R.drawable.speaking)
+        )
 
+        LearningCard(
+            mainColor = Color(0xFFFF8504),
+            shadowColor = Color(0xFF473D33),
+            menu = "Listening",
+            content = "Catch the missing words in the lyrics as you listen!",
+            img = painterResource(id = R.drawable.listening)
+        )
     }
 }
 
 @Composable
-fun LearningCard(){
-    Card {
-        Box(){
-            Row {
+fun LearningCard(mainColor: Color, shadowColor: Color, menu: String, content: String, img: Painter) {
+    Box(
+        modifier = Modifier
+            .padding(bottom = 40.dp)
+    ) {
+        //Shadow
+        Box(
+            modifier = Modifier
+                .width(326.dp)
+                .height(132.dp)
+                .offset(x = 8.dp, y = 8.dp)
+                .background(color = shadowColor, shape = RoundedCornerShape(size = 12.dp))
+        )
 
+        Box(
+            Modifier
+                .width(326.dp)
+                .height(132.dp)
+                .background(color = mainColor, shape = RoundedCornerShape(size = 12.dp))
+        ) {
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp, end = 32.dp)
+                ,
+            ){
+                Image(
+                    painter = img,
+                    contentDescription = "Speaking Img",
+                    modifier = Modifier
+                        .graphicsLayer { scaleX = (-1f).toFloat() }
+                        .padding(end = 10.dp, start = 8.dp)
+                        .size(113.dp)
+                )
+                Column (
+
+                ){
+                    Text(
+                        text = menu,
+                        style = TextStyle(
+                            fontSize = 24.sp,
+                            lineHeight = 31.2.sp,
+                            fontFamily = poppins,
+                            fontWeight = FontWeight.SemiBold,
+                            color = Color(0xFFEFF8FF),
+                        ),
+                        modifier = Modifier
+                            .padding(bottom = 4.dp)
+                    )
+                    Text(
+                        text = content,
+                        style = TextStyle(
+                            fontSize = 10.sp,
+                            lineHeight = 14.sp,
+                            fontFamily = poppins,
+                            fontWeight = FontWeight(500),
+                            color = Color(0xFFFFFFFF),
+                        )
+                    )
+                }
             }
             FloatingActionButton(
                 onClick = { /*TODO*/ },
                 modifier = Modifier
                     .align(Alignment.BottomEnd)
-                    .padding(bottom = 16.dp, end = 16.dp)
+                    .padding(bottom = 10.dp, end = 10.dp)
+                    .clip(CircleShape)
+                    .size(52.dp)
+                ,
+                containerColor = Color.White
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.ic_arrow_right),
                     contentDescription = "Play Button",
                     modifier = Modifier
-                        .size(24.dp)
+                        .size(48.dp)
+                    ,
+                    colorFilter = ColorFilter.tint(mainColor)
                 )
             }
         }
     }
 }
+
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
