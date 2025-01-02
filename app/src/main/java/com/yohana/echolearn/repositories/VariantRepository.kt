2d8b8295035f6f649cea6ddbc639dc8f4f2879be
Daffa954 <dkhoirul05@gmail.com>
2025-01-02 19:11:16 +0700

    updated

--- app/src/main/java/com/yohana/echolearn/repositories/VariantRepository.kt
+++ app/src/main/java/com/yohana/echolearn/repositories/VariantRepository.kt
@@ -1,11 +1,14 @@
 package com.yohana.echolearn.repositories
 
+import com.yohana.echolearn.models.AttemptResponse
+import com.yohana.echolearn.models.SpeakingRequest
 import com.yohana.echolearn.models.VariantListResponse
 import com.yohana.echolearn.services.VariantAPIService
 import retrofit2.Call
 
 interface VariantRepository{
     fun getVariants(songId: Int, type: String): Call<VariantListResponse>
+    fun checkAnswerSpeaking(token: String, variantId:Int, answer: String): Call<AttemptResponse>
 }
 
 class NetworkVariantRepository(
@@ -13,5 +16,8 @@ class NetworkVariantRepository(
 ): VariantRepository{
     override fun getVariants(songId: Int, type: String): Call<VariantListResponse> {
         return variantAPIService.getVariants(songId, type)
+    }
+    override fun checkAnswerSpeaking(token: String, variantId: Int, answer: String): Call<AttemptResponse> {
+        return variantAPIService.checkAnswerSpeaking(token, variantId, SpeakingRequest(answer, variantId))
     }
 }
