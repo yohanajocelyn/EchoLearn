9a95df4c6680e3608f2a192b10501d05d9768eb5
Daffa954 <dkhoirul05@gmail.com>
2025-01-02 18:32:22 +0700

    Update AuthenticationRepository.kt

--- app/src/main/java/com/yohana/echolearn/repositories/AuthenticationRepository.kt
+++ app/src/main/java/com/yohana/echolearn/repositories/AuthenticationRepository.kt
@@ -7,6 +7,7 @@ import retrofit2.Call
 interface AuthenticationRepository{
     fun register(username: String, email: String, password: String, profilePicture: String): Call<UserResponse>
     fun login(email: String, password: String): Call<UserResponse>
+    
     suspend fun getDefaultProfilePictures(): List<String>
 }
 
